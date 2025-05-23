f9d263c752b88be7cc597951eace136e8e6246e2
Sylvain Lehman <sylvain@lehmann.dev>
2025-05-23 16:56:58 +0200

    feature(ui): add help messages

--- lang/fulltextsearch_fr.inc.php
+++ lang/fulltextsearch_fr.inc.php
@@ -1,7 +1,10 @@
 <?php
 
 return [
+    'FULLTEXTSEARCH_INIT_TITLE' => 'Utilisez le bouton ci-dessous pour lancer l\'indexation de la base de données. Cela est normalement necessaire une seule fois à l\'installation de l\'extension.',
+
     'FULLTEXTSEARCH_EXCLUSIONS_LABEL_STATE' => 'Statut',
+    'FULLTEXTSEARCH_EXCLUSIONS_TITLE' => 'Utilisez le tableau ci-dessous pour exclure des pages de la recherche. L\'exclusion est prise en compte immédiatement, il n\'est pas necessaire de ré indexer les pages.',
 
     'FULLTEXTSEARCH_ERROR_ENGINE_NOT_CONFIGURED' => 'Erreur : merci de configurer le moteur d\'indexation avant d\'utiliser la recherche',
 
